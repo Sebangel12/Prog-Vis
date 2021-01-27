3833148823f80260734feea3167509b3636309f9
ANGEL SILVA <78116603+Sebangel12@users.noreply.github.com>
2021-01-27 18:27:57 -0500

    Add files via upload

---
+++ Flota pesquera/CapaDatos/SQLServer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlotaPesquera.Datos
+{
+    public class SQLServer
+    {
+        public string cadenaConexion { get; set; }
+        public string ObtenerDatos (int idBarco)
+        {
+            if (idBarco == 30)
+                return "Titanic";
+            else
+                return "Brisa";
+        }
+               
+    }
+}
---
+++ Flota pesquera/CapaNegocios/Barco.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlotaPesquera.Datos;
+
+
+namespace FlotaPesquera.Negocios
+{
+    public class Barco
+    {
+        public object Dim { get; private set; }
+        
+        public string ConsultarTripulantes (int idBarco)
+        {
+            SQLServer objetoSQL = new SQLServer();
+            string nombreBarco = objetoSQL.ObtenerDatos(idBarco);
+            
+            if (nombreBarco == "Titanic")
+                return "El barco " + nombreBarco + " cuenta con 40 personas";
+            else 
+                return "El barco " + nombreBarco + " cuenta con 30 personas";
+
+        }
+        
+    }
+}
