e5542b5c7588b851234dc026f75de56a774ff707
ANGEL SILVA <78116603+Sebangel12@users.noreply.github.com>
2021-01-27 18:35:36 -0500

    Add files via upload

---
+++ prueba1/prueba1/Form1.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Radio = New System.Windows.Forms.TextBox()
+        Me.Altura = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.resultado = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Radio
+        '
+        Me.Radio.Location = New System.Drawing.Point(325, 39)
+        Me.Radio.Name = "Radio"
+        Me.Radio.Size = New System.Drawing.Size(100, 20)
+        Me.Radio.TabIndex = 0
+        '
+        'Altura
+        '
+        Me.Altura.Location = New System.Drawing.Point(325, 90)
+        Me.Altura.Name = "Altura"
+        Me.Altura.Size = New System.Drawing.Size(100, 20)
+        Me.Altura.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(218, 160)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Label1"
+        '
+        'resultado
+        '
+        Me.resultado.Location = New System.Drawing.Point(169, 119)
+        Me.resultado.Name = "resultado"
+        Me.resultado.Size = New System.Drawing.Size(150, 23)
+        Me.resultado.TabIndex = 3
+        Me.resultado.Text = "Button1"
+        Me.resultado.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(139, 39)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(139, 90)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Label3"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(489, 223)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.resultado)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Altura)
+        Me.Controls.Add(Me.Radio)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Radio As TextBox
+    Friend WithEvents Altura As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents resultado As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ prueba1/prueba1/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+        Dim W, X, Z As Single
+        Const pi As Single = 3.1415
+        X = Val(Radio.Text) / 2
+        Z = Val(Altura.Text)
+        W = pi * (X * X) * Z
+        resultado.Text = " EL VOLUMEN DEL CILINDRO ES DE " & W & " METROS CUBICOS "
+    End Sub
+End Class
