240b3e8728259954b03c6abace00638454a1f00d
ANGEL SILVA <78116603+Sebangel12@users.noreply.github.com>
2021-02-03 16:48:06 -0500

    Delete AreasyPerimetrodeFiguras directory

--- AreasyPerimetrodeFiguras/AreasyPerimetrodeFiguras/Form1.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class FrmPresentacion
-    Private Sub BtnMenu_Click(sender As Object, e As EventArgs) Handles BtnMenu.Click
-        Me.Hide()
-        FrmMenu.Show()
-    End Sub
-End Class
