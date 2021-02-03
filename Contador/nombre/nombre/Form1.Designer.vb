0e34429f8822a635b3818c318d1fc5d2421c817c
ANGEL SILVA <78116603+Sebangel12@users.noreply.github.com>
2021-02-03 16:31:32 -0500

    Add files via upload

---
+++ Contador/nombre/nombre/Form1.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.lblSaludo = New System.Windows.Forms.Label()
+        Me.TxtNombre = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Modern No. 20", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(204, 248)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(154, 37)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Saludar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'lblSaludo
+        '
+        Me.lblSaludo.Enabled = False
+        Me.lblSaludo.Font = New System.Drawing.Font("Modern No. 20", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSaludo.Location = New System.Drawing.Point(95, 213)
+        Me.lblSaludo.Name = "lblSaludo"
+        Me.lblSaludo.Size = New System.Drawing.Size(390, 23)
+        Me.lblSaludo.TabIndex = 1
+        '
+        'TxtNombre
+        '
+        Me.TxtNombre.Location = New System.Drawing.Point(204, 116)
+        Me.TxtNombre.Name = "TxtNombre"
+        Me.TxtNombre.Size = New System.Drawing.Size(154, 20)
+        Me.TxtNombre.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Modern No. 20", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(33, 68)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(253, 29)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "¿Cuál es tu nombre?"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(543, 363)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TxtNombre)
+        Me.Controls.Add(Me.lblSaludo)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents lblSaludo As Label
+    Friend WithEvents TxtNombre As TextBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Contador/nombre/nombre/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+
+    Public nombre As String
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        nombre = TxtNombre.Text
+
+        lblSaludo.Text = ("Hola ") & nombre & (", lindo nombre")
+    End Sub
+End Class
